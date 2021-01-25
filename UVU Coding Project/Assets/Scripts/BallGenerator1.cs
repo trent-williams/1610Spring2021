5ccea7cf5591cbda608b6c5cf77240ccdd8b1cfa
Trent Williams <10848261@my.uvu.edu>
2021-01-25 10:51:36 -0700

    First Script
    adding script to Unity Project

---
+++ UVU Coding Project/Assets/Scripts/BallGenerator1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+    private int number = 0;
+    IEnumerator Start()
+    {
+        while (number < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+			number++;
+        }
+    }
+}
