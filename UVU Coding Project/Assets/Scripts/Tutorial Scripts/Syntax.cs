579325fc99290ec2f836b82cdae1689840eba1c3
Trent Williams <10848261@my.uvu.edu>
2021-01-26 12:10:28 -0700

    Unity Project Back-up
    Due to errors ocuring in the editor, I am trying the 2019 version of Unity instead of 2020. This is a back up of the project before possibly destroying it by shifting versions.

---
+++ UVU Coding Project/Assets/Scripts/Tutorial Scripts/Syntax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UVU Coding Project/Assets/Scripts/Tutorial Scripts/VariableTesting.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+	int myInt = 5;
+
+    void Start()
+    {
+        Debug.Log(myInt);
+    }
+}
