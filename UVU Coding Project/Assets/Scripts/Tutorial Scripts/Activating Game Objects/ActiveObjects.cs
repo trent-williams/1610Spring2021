c59de390f36c106dad8c451818ee9dfe88067105
Trent Williams <10848261@my.uvu.edu>
2021-01-28 11:05:45 -0700

    Unity Beginner Scripting Backup

---
+++ UVU Coding Project/Assets/Scripts/Tutorial Scripts/Activating Game Objects/ActiveObjects.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEngine;
+
+public class ActiveObjects : MonoBehaviour
+{
+    void Start()
+    {
+       gameObject.SetActive(false); 
+    }
+
+}
---
+++ UVU Coding Project/Assets/Scripts/Tutorial Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
