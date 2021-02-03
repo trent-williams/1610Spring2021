1686ee4a6279502c195ac8a4b7e00de2b6c8cc05
Trent Williams <10848261@my.uvu.edu>
2021-02-03 10:51:19 -0700

    Making BB-8
    This is amazing

---
+++ Ball Roller/Assets/Scripts/Syntax.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax : MonoBehaviour
+{
+    public int number = 2;
+
+    public void Run()
+    {
+        number = 4;
+    }
+}
---
+++ Ball Roller/Assets/Scripts/UserInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public float speed = 3f;
+    public float jumpForce = 300f;
+    
+    private Vector2 direction;
+    private Vector2 yDirection;
+
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal") * speed;
+        ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection.y = jumpForce;
+            ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
+        }
+    }
+}
