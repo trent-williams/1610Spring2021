c3e061005fdf56445f3f375d4943ab5142fe8bb2
Trent Williams <10848261@my.uvu.edu>
2021-03-03 11:46:06 -0700

    Creating w/ Code, Prototype 1 Completion

---
+++ Creating With Code/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Creating With Code/Assets/Course Library/Scripts/PlayerController.cs
+++ Creating With Code/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+     // Move the vehicle forward   
+     horizontalInput = Input.GetAxis("Horizontal");
+     forwardInput = Input.GetAxis("Vertical");
+     
+     transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+     //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+     transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
