222fd7d803c69d599f1aabafd260070d185e1494
Trent Williams <10848261@my.uvu.edu>
2021-03-25 14:45:02 -0600

    Lab Updates/ Character Movements and Boundaries

---
+++ Lab 2 - Currently Unknown/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    public float speed = 3f, gravity = -8f, jumpForce = 10f, rearBoundry = 7f;
+    public int jumpCountMax = 2;
+
+    private CharacterController controller;   
+    private Vector3 movement, rotation;
+    private float yDirection;
+    private int jumpCount;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        movement.Set(speed*Input.GetAxis("Vertical"), yDirection,0);
+
+        yDirection += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yDirection = -1f;
+            jumpCount = 0;
+        }
+        
+        //Double jump constraint
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            yDirection = jumpForce;
+            jumpCount++;
+        }
+
+        rotation.y = Input.GetAxis("Horizontal");
+        //transform.Rotate(rotation);
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
+    }
+}
